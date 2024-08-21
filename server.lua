62fd39c5f55a4a0c4c28c12231c2099119751b49
Snov <snov@snov.dev>
2024-08-21 18:10:09 +0200

    update check

--- server.lua
+++ server.lua
@@ -70,3 +70,35 @@ AddEventHandler('onResourceStop', function(resourceName)
         GlobalState.sperrzonen = nil
     end
 end)
+
+function checkVersion(initial)
+    PerformHttpRequest('https://raw.githubusercontent.com/Snovna/fivem_updates/main/updates.json', function(statusCode, responseText, headers)
+        local resourceName = GetCurrentResourceName()
+        local fxVersion = GetResourceMetadata(resourceName, 'version', 0)
+        local gitResponse = json.decode(responseText)
+
+        if statusCode ~= 200 then print('^3version check failed, response error code '..statusCode) return end
+        if not gitResponse then print('^3version check failed') return end
+        if not gitResponse[resourceName] then print('^3version check failed, did you rename this resource?') return end
+        if fxVersion ~= gitResponse[resourceName].currentVersion then
+            print('^3'..resourceName..' is outdated (your version: '..fxVersion..' - current version: '..gitResponse[resourceName].currentVersion..')')
+            print('^3'..gitResponse[resourceName].updateNotes)
+            if gitResponse[resourceName].updateUrl then
+                print('^3update it now from: '..gitResponse[resourceName].updateUrl)
+            else
+                print('^3update it now from your Keymaster')
+            end
+        else
+            if not initial then return end
+            print('^2'..resourceName .. ' is up to date ('..fxVersion..')')
+        end
+    end, 'GET')
+end
+CreateThread(function()
+    Wait(15 * 1000)
+    checkVersion(true)
+    while true do
+        Wait(30 * 60 * 1000)
+        checkVersion(false)
+    end
+end)
