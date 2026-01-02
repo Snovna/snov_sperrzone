a2687060a8e00529590da8b225c7192c2e004da8
Snov <snov@snov.dev>
2026-01-02 22:50:49 +0100

    fix: add color reset to checkVersion prints in snov_sperrzone

--- server.lua
+++ server.lua
@@ -77,20 +77,20 @@ function checkVersion(initial)
         local fxVersion = GetResourceMetadata(resourceName, 'version', 0)
         local gitResponse = json.decode(responseText)
 
-        if statusCode ~= 200 then print('^3version check failed, response error code '..statusCode) return end
-        if not gitResponse then print('^3version check failed') return end
-        if not gitResponse[resourceName] then print('^3version check failed, did you rename this resource?') return end
+        if statusCode ~= 200 then print('^3version check failed, response error code '..statusCode..'^0') return end
+        if not gitResponse then print('^3version check failed^0') return end
+        if not gitResponse[resourceName] then print('^3version check failed, did you rename this resource?^0') return end
         if fxVersion ~= gitResponse[resourceName].currentVersion then
-            print('^3'..resourceName..' is outdated (your version: '..fxVersion..' - current version: '..gitResponse[resourceName].currentVersion..')')
-            print('^3'..gitResponse[resourceName].updateNotes)
+            print('^3'..resourceName..' is outdated (your version: '..fxVersion..' - current version: '..gitResponse[resourceName].currentVersion..')^0')
+            print('^3'..gitResponse[resourceName].updateNotes..'^0')
             if gitResponse[resourceName].updateUrl then
-                print('^3update it now from: '..gitResponse[resourceName].updateUrl)
+                print('^3update it now from: '..gitResponse[resourceName].updateUrl..'^0')
             else
-                print('^3update it now from your Keymaster')
+                print('^3update it now from your Keymaster^0')
             end
         else
             if not initial then return end
-            print('^2'..resourceName .. ' is up to date ('..fxVersion..')')
+            print('^2'..resourceName .. ' is up to date ('..fxVersion..')^0')
         end
     end, 'GET')
 end
